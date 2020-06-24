3983344edeece081e97ae9188720c299224ca947
Hillary Reis <hillary.reis2002@gmail.com>
2020-06-24 16:34:05 -0300

    sexta versao (add as fotos e o modal)

--- scripts.js
+++ scripts.js
@@ -1,10 +1,19 @@
 const modalPhotos = document.querySelector ('#modal-photos');
-const modalImg = modalPhotos.querySelectorAll ('.modal-img');
+const modalImg = modalPhotos.querySelector ('.modal-img');
 const photos = document.querySelectorAll ('.photo');
 
+
 for (let photo of photos) {
-  photo.addEventListener ("mouseouver", function () {
-    const image = photo.querySelector ('.photo').innerHTML
+  photo.addEventListener ("dblclick", function () {
+    const image = photo.innerHTML;
+
     modalPhotos.querySelector ('.modal-img').innerHTML = image;
+
+    modalPhotos.classList.add ('active');
   });
 };
+
+
+document.querySelector ('.close-modal').addEventListener ('click', function () {
+  modalPhotos.classList.remove ('active');
+});
